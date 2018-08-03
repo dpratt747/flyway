a4e139ba068a349fe56007ec227ee8ea8a480d05
pratdavi <dpratt747@gmail.com>
2018-08-03 01:57:26 +0100

    added fk constraints to tables.

--- migrations/sql/V1__Create_brand_table.sql
+++ migrations/sql/V1__Create_brand_table.sql
@@ -1,12 +1,24 @@
 CREATE TABLE IF NOT EXISTS brands (
     BrandID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    BrandName VARCHAR(100) NOT NULL
+    BrandName VARCHAR(100) NOT NULL,
+    TypeID INT NOT NULL,
+    ContactDetailsID INT
 );
 
-CREATE TABLE IF NOT EXISTS brandType (
+CREATE TABLE IF NOT EXISTS brand_type (
     TypeID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     TypeName VARCHAR(100) NOT NULL
 );
 
-ALTER TABLE brand
-    ADD FOREIGN KEY (TypeID_FK) REFERENCES brandType(TypeID);
+CREATE TABLE IF NOT EXISTS brand_contact_details (
+    ContactID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    PhoneNumber VARCHAR(100),
+    Address VARCHAR(100)
+);
+
+ALTER TABLE brands
+    ADD FOREIGN KEY (TypeID) REFERENCES brand_type(TypeID);
+
+ALTER TABLE brands
+    ADD FOREIGN KEY (ContactDetailsID) REFERENCES brand_contact_details(ContactID);
+
